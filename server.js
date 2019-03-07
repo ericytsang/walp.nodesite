709db1501f41c354d1002c6e7c6f823606371489
Eric Tsang <surplus.et@gmail.com>
2019-03-07 13:21:24 -0800

    comments

--- server.js
+++ server.js
@@ -2,10 +2,14 @@ const express = require("express");
 const app = express();
 const port = 9090
 
+// server
+
 app.listen(port)
 {
   console.log("listening on "+port)
 }
+
+// practising with GET
 
 app.get("/", (req, res) =>
 {
