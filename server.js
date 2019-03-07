26acdf9e87a8a64eb083af94eb76c7004270f885
Eric Tsang <surplus.et@gmail.com>
2019-03-07 13:25:13 -0800

    connect to mongodb

--- server.js
+++ server.js
@@ -6,12 +6,28 @@ const port = 9090
 
 app.use(bodyParser.urlencoded({extended: true}))
 
-// server
+const mongoDbClient = require('mongodb').MongoClient
+var mongoDbConnection;
 
-app.listen(port)
-{
-  console.log("listening on "+port)
-}
+// connect to database
+
+mongoDbClient.connect('mongodb+srv://application:4OoNV5z8Jw5ac0B0VKCIl6sMa6h@walp-nodesite-pgjky.mongodb.net/test?retryWrites=true', (err, client) => {
+
+  // log error if present
+  if (err)
+  {
+    return console.log(err)
+  }
+
+  // establish connection with database
+  mongoDbConnection = client.db("walp-nodesite")
+
+  // only listen for web client requests after connection with db is established
+  app.listen(port)
+  {
+    console.log("listening on "+port)
+  }
+})
 
 // practising with GET
 
